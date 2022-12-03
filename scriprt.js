65561a245bb7cef18b28a6180818077e767eb9bd
T10AA <113979047+T10AA@users.noreply.github.com>
2022-12-03 19:27:43 +0400

    finished

--- scriprt.js
+++ scriprt.js
@@ -37,9 +37,52 @@ NameYear();
 
 
 // Homework N2
-function users () {
-    let request = XMLHttpRequest();
-    request.addEventListener('load', function() {
-        
-    })
+
+let currentPage = 1;
+let totalPages;
+
+function Users(page) {
+    let request = new XMLHttpRequest();
+
+request.addEventListener('load', function() {
+    let gotInfoText = request.responseText;
+    let gotIfoJs = JSON.parse(gotInfoText);
+
+    const fragment = new DocumentFragment();
+    gotIfoJs.data.forEach(item => {
+        let li = document.createElement('li');
+        li.innerText = `${item.first_name} ${item.last_name}`;
+        li.classList = 'li-xml';
+        fragment.appendChild(li);  
+    });
+    document.getElementById('userList').innerHTML = ' ';
+    document.getElementById('userList').appendChild(fragment);
+
+    totalPages = gotIfoJs.total_pages;
+});
+
+request.addEventListener('error', function () {
+    let p = document.createElement('p');
+    p.innerText = 'Server Error';
+    document.getElementById('userList').appendChild(p);
+});
+
+request.open('GET', 'https://reqres.in/api/users?page=' + page);
+request.send();
 }
+
+document.getElementById('Previous').addEventListener('click' , function() {
+    if (currentPage == 1) {
+        return;
+    }
+    currentPage --;
+    Users(currentPage);
+});
+document.getElementById('Next').addEventListener('click' , function() {
+    if (currentPage == totalPages) {
+        return;
+    }
+    currentPage ++;
+    Users(currentPage);
+});
+Users(currentPage);
